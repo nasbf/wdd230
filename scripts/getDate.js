7624b295506d822a53b45bdb8083e3e8f98920a6
nasbf <mo0ma@hotmail.com>
2025-03-15 23:12:03 -0500

    update

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -7,9 +7,31 @@ info.textContent = `© ${currentYear} Alexandra Suárez Bogotá - Colombia`;
 const modifiedDate = document.lastModified;
 modified.textContent = `Last modification: ${modifiedDate}`;
 
-const navlist = document.querySelector('.menu');
-const hamburguer = document.querySelector('#hamb');
-hamburguer.addEventListener('click', () => {
-    navlist.classList.toggle('display');
-    hamburguer.classList.toggle('display'); 
+/*Hamburguer menu*/
+
+const menuList = document.getElementById('menuList');
+const menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('show');
+    menuBtn.classList.toggle('show');
 })
+
+/*dark mode*/
+
+const bgrbtnDiv = document.getElementById("bgrbtn-div");
+const main = document.querySelector("main");
+const firstTitle = document.querySelector('#firstTitle');
+
+bgrbtnDiv.addEventListener("click", () => {
+    if (bgrbtnDiv.textContent.includes("🌓")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        firstTitle.style.color = "white";
+
+        bgrbtnDiv.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        bgrbtnDiv.textContent = "🌓";
+    }
+});
